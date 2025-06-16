ee135ee2833d317bedf207b3b03dc13fb010a12a
Akash Trivedi <akash.trivedi@fishtownanalytics.com>
2025-06-16 10:32:17 +0000

    fixed issue with dbt_env_var not defined and wrong schema name for audit helper test

--- tests/_samples/audit_helper_compare_all_columns__stg_tpch__orders.sql
+++ tests/_samples/audit_helper_compare_all_columns__stg_tpch__orders.sql
@@ -9,7 +9,7 @@
 {{ 
   audit_helper.compare_all_columns(
     a_relation=ref('stg_tpch__orders'),
-    b_relation=api.Relation.create(database='analytics', schema='analytics', identifier='stg_tpch__orders'),
+    b_relation=api.Relation.create(database='analytics', schema='dbt_atrivedi', identifier='stg_tpch__orders'),
 
     primary_key='order_id'
   ) 
